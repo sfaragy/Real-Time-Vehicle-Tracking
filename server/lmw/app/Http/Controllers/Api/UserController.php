312f1e0773bfef1f5b9958499e82767318a08a55
sfaragy <solimankhulna@gmail.com>
2024-04-18 22:48:23 +0700

    Add missing import

--- server/lmw/app/Http/Controllers/Api/UserController.php
+++ server/lmw/app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
